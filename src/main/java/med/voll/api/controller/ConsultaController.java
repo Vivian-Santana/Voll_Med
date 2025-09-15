5e79bd61335fbd537b00c804bb5242ba872ce67c
Vivian Santana <santanavivian245@gmail.com>
2025-09-14 22:11:20 -0300

    Ajuste no endpoint cancelar consulta e em listar consultas para mostrar apenas consultas ativas

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,6 +1,10 @@
 package med.voll.api.controller;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Stream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,16 +17,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import med.voll.api.domain.consulta.AgendaDeConsultas;
+import med.voll.api.domain.consulta.Consulta;
 import med.voll.api.domain.consulta.ConsultaRepository;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import med.voll.api.domain.consulta.MotivoCancelamento;
 import med.voll.api.domain.paciente.Paciente;
 import med.voll.api.domain.paciente.PacienteRepository;
 import med.voll.api.domain.usuario.Usuario;
@@ -53,6 +60,17 @@ public class ConsultaController {
         return ResponseEntity.noContent().build();
     }
     
+    @DeleteMapping("/{id}")
+    @Transactional
+    @PreAuthorize("hasRole('PACIENTE')")
+    public ResponseEntity cancelar(
+            @PathVariable Long id,
+            @RequestParam MotivoCancelamento motivo) {
+        agenda.cancelar(new DadosCancelamentoConsulta(id, motivo));
+        return ResponseEntity.noContent().build();
+    }
+
+    
     @GetMapping("/{id}/consultas")
     @PreAuthorize("hasRole('ADMIN') or @pacienteRepository.findById(#id).get().usuario.id == principal.id")
     public ResponseEntity<List<DadosDetalhamentoConsulta>> listarConsultas(@PathVariable Long id) {
@@ -75,11 +93,16 @@ public class ConsultaController {
         Usuario usuario = (Usuario) auth.getPrincipal();
         Paciente paciente = pacienteRepository.findByUsuarioId(usuario.getId())
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        
+        LocalDateTime inicioHoje = LocalDate.now().atStartOfDay();
+        LocalDateTime fimInfinito = LocalDateTime.of(3000, 1, 1, 0, 0);
+
         List<DadosDetalhamentoConsulta> consultas = consultaRepository
-                .findByPacienteId(paciente.getId())
+        		.buscarAtivasEFuturas(paciente.getId(), inicioHoje, fimInfinito)
                 .stream()
                 .map(DadosDetalhamentoConsulta::new)
                 .toList();
+        
         return ResponseEntity.ok(consultas);
     }
 
--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -1,7 +1,10 @@
 package med.voll.api.domain.consulta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -11,5 +14,16 @@ public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 
     boolean existsByPacienteIdAndDataBetween(Long idPaciente, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
     
-    List<Consulta> findByPacienteId(Long idPaciente); // <-- novo método
+    List<Consulta> findByPacienteId(Long idPaciente);
+    
+    LocalDateTime inicioHoje = LocalDate.now().atStartOfDay();
+    LocalDateTime fimInfinito = LocalDateTime.of(3000, 1, 1, 0, 0); // ou qualquer data muito no futuro
+    
+    @Query("SELECT c FROM Consulta c"
+    		+ " WHERE c.paciente.id = :pacienteId "
+    		+ "AND c.motivoCancelamento IS NULL "
+    		+ "AND c.data BETWEEN :inicioHoje AND :fimInfinito")
+    List<Consulta> buscarAtivasEFuturas(@Param("pacienteId") Long pacienteId,
+    									@Param("inicioHoje") LocalDateTime inicioHoje, 
+    									@Param("fimInfinito") LocalDateTime fimInfinito);
 }
